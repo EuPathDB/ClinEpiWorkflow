4322eff0464aefa008673d2099f9ac86ee794414
Danielle Callan <dcallan@upenn.edu>
2018-07-25 22:08:57 +0000

    bug where logic to check for errors in reading file was reversed
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWorkflow/trunk@90357 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/bin/mergeClinEpiShinyDatasetFiles.R
+++ Main/bin/mergeClinEpiShinyDatasetFiles.R
@@ -71,6 +71,8 @@ for (i in 1:length(shinyFiles)) {
 fwrite(masterDataTable, file.path(dataDir,"shiny_masterDataTable.txt"), sep='\t', na="NA")
 
 #ask JohnB if he wants the ontology file in downloadDir also
+metadata.temp <- fread(paste0(dataDir, "/ontologyMetadata.txt"))
+
 if (!any(grepl("Error", metadata.temp[1]))) {
   metadata.file <- metadata.temp
   names(metadata.file) <- tolower(names(metadata.file))
