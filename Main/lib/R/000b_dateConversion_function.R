9367200540595c601b75cd516d52caa8afca5c46
Danica Helb <dhelb@upenn.edu>
2019-06-12 16:23:48 +0000

    000b_dateConversion_function.R updated so that data with non-date/date-ish formats (ex: 00/Jul/2017) are not converted
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWorkflow/trunk@98923 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Main/lib/R/000b_dateConversion_function.R
+++ Main/lib/R/000b_dateConversion_function.R
@@ -11,20 +11,22 @@ dateConversion <- function(dateDF, allVarsDF, dataFilesDF) {
         temp.values <- gsub(":00:00:00", "", temp.values)
         temp.values <- sort(as.Date(temp.values, format="%d%b%y"), na.last=T)
         
-        allVarsDF[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i, "minValue"] <- 
-          as.character(as.Date(min(temp.values[!is.na(temp.values)])))
-        allVarsDF[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i, "maxValue"] <- 
-          as.character(as.Date(max(temp.values[!is.na(temp.values)])))
-        allVarsDF[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i, "values"] <- 
-          paste(allVarsDF[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i,"minValue"], 
-                allVarsDF[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i,"maxValue"], sep=" to ")
+        if(length(temp.values[!is.na(temp.values)])>0){
+          allVarsDF[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i, "minValue"] <- 
+            as.character(as.Date(min(temp.values[!is.na(temp.values)])))
+          allVarsDF[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i, "maxValue"] <- 
+            as.character(as.Date(max(temp.values[!is.na(temp.values)])))
+          allVarsDF[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i, "values"] <- 
+            paste(allVarsDF[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i,"minValue"], 
+                  allVarsDF[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i,"maxValue"], sep=" to ")
+        }
       }
       
       if (!is.na(allVarsDF$formatCode[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i]) & 
           allVarsDF$formatCode[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i]!="%d%b%y:%h:%m:%s"){
         formatCode <- allVarsDF$formatCode[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i]
         temp.values <- sort(as.Date(unique(as.vector(unlist(dataFilesDF[[i]][j]))), format=formatCode), na.last=T)
-        
+
         if(length(temp.values[!is.na(temp.values)])>0){
           allVarsDF[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i, "minValue"] <- 
             as.character(as.Date(min(temp.values[!is.na(temp.values)])))
@@ -37,7 +39,6 @@ dateConversion <- function(dateDF, allVarsDF, dataFilesDF) {
       }
     }
   }
-  
   return(allVarsDF)
 }
 
@@ -48,18 +49,18 @@ dateConversion_DF <- function(dateDF, allVarsDF, dataFilesDF) {
     vars <- dateDF$variable[dateDF$dataFile==i]
     
     for (j in vars){
-      if (!is.na(allVarsDF$formatCode[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i]) & 
-          allVarsDF$formatCode[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i]=="%d%b%y:%h:%m:%s"){
-        temp.values <- as.vector(unlist(dataFilesDF[[i]][j]))
-        temp.values <- gsub(":00:00:00", "", temp.values)
-        dataFilesDF[[i]][j] <- as.Date(temp.values, format="%d%b%y")
+      formatCode <- allVarsDF$formatCode[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i]
+      temp <- data.frame(values=gsub(":00:00:00", "", as.vector(unlist(dataFilesDF[[i]][j]))),
+                         dates=as.Date(gsub(":00:00:00", "", as.vector(unlist(dataFilesDF[[i]][j]))), format=formatCode))
+      temp$values <- as.character(temp$values)
+      
+      if(length(temp[is.na(temp$dates) & !is.na(temp$values) & temp$values!="","dates"])>0){
+        temp$dates <- as.character(temp$dates)
+        temp[is.na(temp$dates) & !is.na(temp$values) & temp$values!="","dates"] <- temp[is.na(temp$dates) & !is.na(temp$values) & temp$values!="","values"]
+        temp[is.na(temp$dates) & !is.na(temp$values) & temp$values=="","dates"] <- ""
       }
       
-      if (!is.na(allVarsDF$formatCode[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i]) & 
-          allVarsDF$formatCode[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i]!="%d%b%y:%h:%m:%s"){
-        formatCode <- allVarsDF$formatCode[!is.na(allVarsDF$variable) & allVarsDF$variable==j & allVarsDF$dataFile==i]
-        dataFilesDF[[i]][j] <- as.Date(as.vector(unlist(dataFilesDF[[i]][j])), format=formatCode)
-      }
+      dataFilesDF[[i]][j] <- temp$dates
     }
   }
   
