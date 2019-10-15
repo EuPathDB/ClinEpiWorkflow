988d85a1b026b80635071ae6db9e7e988055ba11
SheenaTomko <stomko@sas.upenn.edu>
2019-10-15 15:55:33 -0400

    Updates to R functions for creating and updating variableMap files

--- Main/lib/R/0003_SST_makeVariableMap.R
+++ Main/lib/R/0003_SST_makeVariableMap.R
@@ -1,31 +1,39 @@
 makeVariableMap <- function(allVar, OUTPUT) {
 
-#Remove all variables marked as "discard". Add columns for label and IRI. Remove
-#columns max and min. Go through the updated file manually and fill out the 
-#“label”, “IRI”, and “category” where known. Also fill out termType for all 
-#variables using the following options: multifilter = multifilter category, 
-#value=multifilter value, variable=maps to a variable, category=no data values. 
+#Removes all variables marked as "discard". Adds columns for label, IRI, 
+#parentLabel, and parentIRI. Removes columns max and min. 
+    
+#Go through the updated file manually and fill out the “label”, “IRI”, and 
+#“category” where known. Also fill out termType for all variables using the 
+#following options: variable = maps to a variable, value = multifilter value,
+#multifilter = multifilter category, category = no data values. 
+    
+#Fill out parentLabel and parentIRI for new variables that don't have an 
+#existing IRI. You don't need to fill out parentLabel or parentIRI for variables
+#that map to known ontology terms. Create a new row for any new parentLabels 
+#where the new parentLabel is placed under the "label" column and fill out 
+#parentLabel and parentIRI to help place the new category in the hierarchy.
 
 #REMOVE VARIABLES MARKED AS "DISCARD" #########################################
-allVar2 <- allVar[!(allVar$keepDiscard == "discard"),]
+varMap <- allVar[!(allVar$keepDiscard == "discard"),]
 ###############################################################################
 
 #INITIALIZE NEW COLUMNS #######################################################
-newCol <- c("IRI", "label")
-allVar2[,newCol] <- ""
+newCol <- c("IRI", "label", "parentIRI", "parentLabel")
+varMap[,newCol] <- ""
 ###############################################################################
 
 #ORDER COLUMNS ################################################################
-allVar2 <- allVar2[c("colOrder","variable","dataFile","IRI","label",
+varMap <- varMap[c("colOrder","variable","dataFile","IRI","label",
                      "definition","category","codebookDescription",
-                     "codebookValues","termType","notesForOnt",
+                     "codebookValues","termType","parentIRI","parentLabel","notesForOnt",
                      "notesForProvider","notesForDL","variable_dataFile","uniqueVar",
                      "type","example","dateTime","formatCode","values",
                      "uniqueValueCount","percentMissing","flag")]
 ###############################################################################
 
 #SAVE UPDATED ALLVARIABLES FILE ###############################################
-write.csv(allVar2, OUTPUT, row.names = F)
+write.csv(varMap, OUTPUT, row.names = F)
 ###############################################################################
 
 }
