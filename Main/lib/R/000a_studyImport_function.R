d507ef630effc2043990f5f94a7126ef6614df0d
danicahelb <helbda@yahoo.com>
2020-03-24 11:58:50 -0700

    update 0001_studyImport_function.R to account for no PID

--- Main/lib/R/000a_studyImport_function.R
+++ Main/lib/R/000a_studyImport_function.R
@@ -188,35 +188,41 @@ studyImport <- function(FOLDER, TYPE, STUDY, MISSING, DATE_TIME, PARTICIPANT_ID)
   allVars$timeVarying <- NA
   
   for(i in allVars$uniqueVar){
-    print(i)
-    variable <- unique(allVars$variable[allVars$uniqueVar==i])
-    dataFile <- unique(allVars$file[allVars$uniqueVar==i])
     
-    if(variable %in% tolower(names(dataFiles[[dataFile]]))){
-      temp <- dataFiles[[dataFile]]
-      names(temp) <- tolower(names(temp))
-      temp2 <- temp[,c(PARTICIPANT_ID, variable)]
-      temp2 <- distinct(temp2[!is.na(temp2[,variable]),]) 
+    if(PARTICIPANT_ID!="not applicable"){
       
-      if(length(temp2[,variable])>0){
-        temp3 <- data.frame(table(temp2[,PARTICIPANT_ID]))
+      print(i)
+      variable <- unique(allVars$variable[allVars$uniqueVar==i])
+      dataFile <- unique(allVars$file[allVars$uniqueVar==i])
+      
+      if(variable %in% tolower(names(dataFiles[[dataFile]]))){
+        temp <- dataFiles[[dataFile]]
+        names(temp) <- tolower(names(temp))
+        temp2 <- temp[,c(PARTICIPANT_ID, variable)]
+        temp2 <- distinct(temp2[!is.na(temp2[,variable]),]) 
         
-        if(max(temp3$Freq)==1){
-          allVars$timeVarying[allVars$uniqueVar==i] <- "once"
+        if(length(temp2[,variable])>0){
+          temp3 <- data.frame(table(temp2[,PARTICIPANT_ID]))
+          
+          if(max(temp3$Freq)==1){
+            allVars$timeVarying[allVars$uniqueVar==i] <- "once"
+          }
+          if(max(temp3$Freq)>1){
+            allVars$timeVarying[allVars$uniqueVar==i] <- "repeated"
+          }
         }
-        if(max(temp3$Freq)>1){
-          allVars$timeVarying[allVars$uniqueVar==i] <- "repeated"
+        
+        if(length(temp2[,variable])==0){
+          allVars$timeVarying[allVars$uniqueVar==i] <- "no data"
         }
       }
-      
-      if(length(temp2[,variable])==0){
-        allVars$timeVarying[allVars$uniqueVar==i] <- "no data"
-      }
+      rm(temp, temp2, temp3)
     }
   }
   
-  rm(temp, temp2, temp3, i)
+  rm(i)
   
+
   #############################################################
   # create "flag" and flag variables that are duplicated or missing 100% of data
   
