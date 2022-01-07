b4d4e8a0ef149b251c6af9d70be7e7282eb461f8
SheenaTomko <stomko@sas.upenn.edu>
2022-01-07 12:53:32 -0600

    update data dictionary conversion script

--- Main/lib/R/0002a_WS_convert_xlsx_dd_1_sheet.R
+++ Main/lib/R/0002a_WS_convert_xlsx_dd_1_sheet.R
@@ -1,16 +1,14 @@
-pacman::p_load(
+xl_sheet_dataDict<-function(path){
+  pacman::p_load(
     tidyverse,
     plyr,
     readxl)
-dataDictionary<-function(path){
   read_excel_allsheets <- function(path, tibble = FALSE) {
     sheets <- readxl::excel_sheets(path)
     x <- lapply(sheets, function(X) readxl::read_excel(path, sheet = X))
     if(!tibble) x <- lapply(x, as.data.frame)
     names(x) <- sheets
     x}
-  dc<-rbind.fill(read_excel_allsheets(path))%>%
-     add_column(Missing_values=NA,
-                Format_values=NA) 
+  dc<-rbind.fill(read_excel_allsheets(path))
 }
 
